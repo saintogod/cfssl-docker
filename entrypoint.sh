4ab9745585a2146f0503da424f2e7fb91f2ba526
Neos Santos <saintogod@users.noreply.github.com>
2023-02-28 09:09:27 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,12 @@ if [ ! -f "${CA_CERTS}/certs.db" ]; then
 fi
 
 # Run service
-exec cfssl ocspserve -port=8889 -db-config db-pg.json
-    -db-config "${CA_CONFIG}/db-config.json" \
+# Serving OCSP
+cfssl ocspserve -port=8889 -db-config db-pg.json
+    -db-config "${CA_CONF}/db-config.json" \
     -loglevel 0 &
 
+# Serving Main
 exec /go/bin/cfssl serve -address=0.0.0.0 -port=8888 \
     -config "${CA_CONF}/ca-config.json" \
     -db-config "${CA_CONF}/db-config.json" \
